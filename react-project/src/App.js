b6779f2f5123cc1b25f10553030f4e834c270007
David A. <avila_david_2@yahoo.com>
2024-07-17 16:09:19 -0500

    Updated files
    Configured and attached Spotify API. Currently working on setting up new functions.

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,16 +1,100 @@
 import logo from './compact-disc-solid.svg';
 import './App.css';
-import Player from './SpotifyPlayer'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const CLIENT_ID = "b3016ae17633430fb6949639de1861fe";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+  const [track, setTrack] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    });
+
+    setArtists(data.artists.items);
+  };
+
+  //Get the Current Playing Track
+  const getCurrentTrack = async (e) => {
+    e.preventDefault();
+    const {data} = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    setTrack(data.track.items);
+  };
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
+
+  const renderTrack = () => {
+    {this.getCurrentTrack()}
+
+    return track.map(curTrack => (
+        <div key={curTrack.id}>
+            {curTrack.images.length ? <img width={"100%"} src={curTrack.images[0].url} alt=""/> : <div>No Image</div>}
+            {curTrack.name}
+        </div>
+    ))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Player />
         <p>
           Welcome to Waynes Music Player
         </p>
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
+        {renderArtists()}
+        {renderTrack()}
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button onClick={logout}>Logout</button>}
       </header>
     </div>
   );
