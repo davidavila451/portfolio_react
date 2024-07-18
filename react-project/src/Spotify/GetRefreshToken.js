a7f0d30dea4366778e7440ad656aa4469421148a
David A. <avila_david_2@yahoo.com>
2024-07-18 16:10:46 -0500

    Working on POST
    Working on POST for authentication codes

--- react-project/src/Spotify/GetRefreshToken.js
+++ react-project/src/Spotify/GetRefreshToken.js
@@ -1,12 +1,28 @@
 import {useEffect, useState} from 'react';
+import {Buffer} from 'buffer';
+import querystring from 'query-string';
 import axios from 'axios';
 
 function GetRefreshToken(){
+    const refreshToken = async () => {
+        var scope = 'user-read-private user-read-email'
+        var redirect_uri = "http://localhost:3000";
+
+        console.log(
+            await axios.post(
+                'https://accounts.spotify.com/api/token',
+                {
+                    code: code,
+                    redirect_uri: redirect_uri,
+                    grant_type: 'authorization_code'
+                }
+            )
+        );
+    }
 
     return(
         <div>
-            <p>A Button</p>
-            <button>Get Refresh Token</button>
+            <button onClick={refreshToken}>Get Refresh Token</button>
         </div>
     );
 }
